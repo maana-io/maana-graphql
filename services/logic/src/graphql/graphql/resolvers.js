06695ecf019702d410ebb8e1e9beb1624e873477
witt3rd <witt3rd@witt3rd.com>
2019-03-14 18:27:13 -0700

    basic request working

--- services/logic/src/graphql/graphql/resolvers.js
+++ services/logic/src/graphql/graphql/resolvers.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
-
 import uuid from 'uuid'
 import pubsub from '../../pubsub'
 
 import { log, print } from 'io.maana.shared'
 
 import { rawRequest } from 'graphql-request'
+
+require('dotenv').config()
 
 const SELF = `${process.env.SERVICE_ID}-graphql`
 
@@ -21,37 +21,26 @@ const SELF = `${process.env.SERVICE_ID}-graphql`
 //     }
 //   }
 
-const query = async (endpoint, query) => {
-  const { data, errors, extensions, headers, status } = await rawRequest(
-    endpoint,
-    query
-  )
+const request = async (endpoint, query) => {
+  const results = await rawRequest(endpoint, query)
+  const { data, errors, extensions, headers, status } = results
   console.log(
     JSON.stringify({ data, errors, extensions, headers, status }, undefined, 2)
   )
-}
-
-const mutate = async (endpoint, mutation) => {
-  const { data, errors, extensions, headers, status } = await rawRequest(
-    endpoint,
-    mutation
-  )
-  console.log(
-    JSON.stringify({ data, errors, extensions, headers, status }, undefined, 2)
-  )
+  return results
 }
 
 export const resolver = {
   Query: {
     query: async (_, { endpoint, query }) => {
-      const result = await query(endpoint, query)
+      const result = await request(endpoint, query)
       // pubsub.publish('onQuery', { onQuery: { endpoint, query } })
       return result
     }
   },
   Mutation: {
     mutate: async (_, { endpoint, mutation }) => {
-      const result = await mutate(endpoint, mutation)
+      const result = await request(endpoint, mutation)
       // pubsub.publish('onMutate', { onMutate: { endpoint, mutation } })
       return result
     }
